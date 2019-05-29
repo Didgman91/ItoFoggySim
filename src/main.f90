26f8bb553e99addebb84a3327583e6492ff374d5
itodaiber <st147783@stud.uni-stuttgart.de>
2019-05-29 18:04:41 +0200

    add function to calculate the differential scattering cross section (refere: Light Scattering by Particles Computational Methods -> program S2)

--- src/main.f90
+++ src/main.f90
@@ -6,16 +6,27 @@ program main
     use lib_sort
 
     use lib_ml_fmm
-
     use ml_fmm_math
-
-
     use lib_ml_fmm_type_operator
+
+    use light_scattering
     implicit none
 
     integer :: error_counter
 
 
+!    polarization: parallel (1) perpendicular (2)
+!    size parameter: x
+!    index of refraction: real,imaginary (+ for absorption)
+!    npnts: number of grid points (npnts by npnts)
+!
+!    e.g.:
+!        ip = 1        ! polarisation
+!        x = 2*pi * 10 ! sphere radius: a=10um;  wave length: 1um; a / wave length = 10
+!        cmr = 1.33    ! water: 1.33
+!        cmi = 0       ! water: 0
+!        npnts = 10
+    call S2(1, 2*3.14159265358979*10, 1.33, 0.0, 10)
 
     error_counter = 0
     error_counter = error_counter + lib_sort_test_functions()
