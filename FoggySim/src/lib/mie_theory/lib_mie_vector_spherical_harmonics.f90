b309dfeafc47c83274441dcdf2a0000c319f8dd5
itodaiber <st147783@stud.uni-stuttgart.de>
2019-07-01 15:55:35 +0200

    bug fix: calculation of the components N_emn and N_omn; add function with a complex rho (complex refractive index of the medium)

--- FoggySim/src/lib/mie_theory/lib_mie_vector_spherical_harmonics.f90
+++ FoggySim/src/lib/mie_theory/lib_mie_vector_spherical_harmonics.f90
@@ -11,6 +11,7 @@ module lib_mie_vector_spherical_harmonics
 
     interface lib_mie_vector_spherical_harmonics_components
         module procedure lib_mie_vector_spherical_harmonics_components_real
+        module procedure lib_mie_vector_spherical_harmonics_components_cmplx
     end interface
 
     public :: lib_mie_vector_spherical_harmonics_test_functions
@@ -22,7 +23,7 @@ module lib_mie_vector_spherical_harmonics
 
     contains
 
-        ! calculation of the vector spherical harmonic M_emn
+        ! calculation of the components of the vector spherical harmonic
         !
         ! Argument
         ! ----
@@ -237,6 +238,7 @@ module lib_mie_vector_spherical_harmonics
 
             ! --- calculations of the components M and N ---
             ! M_emn
+            ! eq. (4.17)
             if (.not. m_not_calc_Memn) then
                 select case (z_selector)
                     case (1,2)
@@ -271,6 +273,7 @@ module lib_mie_vector_spherical_harmonics
             end if
 
             ! M_omn
+            ! eq. (4.18)
             if (.not. m_not_calc_Momn) then
                 select case (z_selector)
                     case (1,2)
@@ -305,6 +308,7 @@ module lib_mie_vector_spherical_harmonics
             end if
 
             ! N_emn
+            ! eq. (4.19)
             if (.not. m_not_calc_Nemn) then
                 select case (z_selector)
                     case (1,2)
@@ -317,7 +321,7 @@ module lib_mie_vector_spherical_harmonics
                                 buffer_real = cos_m_phi(i) * p_dmn(i, ii) / rho * r_d_real(ii)
                                 N_emn(i)%coordinate(ii)%theta = cmplx(buffer_real, 0, kind=8)
 
-                                buffer_real = - i * sin_m_phi(i) * p_mn(i, ii) / (sin_theta * rho) * r_d_real(ii)
+                                buffer_real = - i * sin_m_phi(i) * p_mn_divided_by_sin_theta(i, ii) / rho * r_d_real(ii)
                                 N_emn(i)%coordinate(ii)%phi = cmplx(buffer_real,0, kind=8)
                             end do
                         end do
@@ -333,14 +337,16 @@ module lib_mie_vector_spherical_harmonics
                                 buffer_cmplx = buffer_real * r_d_cmplx(ii)
                                 N_emn(i)%coordinate(ii)%theta = buffer_cmplx
 
-                                buffer_real = - i * sin_m_phi(i) * p_mn(i, ii) / (sin_theta * rho)
+                                buffer_real = - i * sin_m_phi(i) * p_mn_divided_by_sin_theta(i, ii) / rho
                                 buffer_cmplx = buffer_real * r_d_cmplx(ii)
                                 N_emn(i)%coordinate(ii)%phi = buffer_cmplx
                             end do
                         end do
                 end select
             end if
+
            ! N_omn
+           ! eq. (4.20)
             if (.not. m_not_calc_Nomn) then
                 select case (z_selector)
                     case (1,2)
@@ -353,7 +359,7 @@ module lib_mie_vector_spherical_harmonics
                                 buffer_real = sin_m_phi(i) * p_dmn(i, ii) / rho * r_d_real(ii)
                                 N_omn(i)%coordinate(ii)%theta = cmplx(buffer_real, 0, kind=8)
 
-                                buffer_real = i * cos_m_phi(i) * p_mn(i, ii) / (sin_theta * rho) * r_d_real(ii)
+                                buffer_real = i * cos_m_phi(i) * p_mn_divided_by_sin_theta(i, ii) / rho * r_d_real(ii)
                                 N_omn(i)%coordinate(ii)%phi = cmplx(buffer_real,0, kind=8)
                             end do
                         end do
@@ -369,7 +375,7 @@ module lib_mie_vector_spherical_harmonics
                                 buffer_cmplx = buffer_real * r_d_cmplx(ii)
                                 N_omn(i)%coordinate(ii)%theta = buffer_cmplx
 
-                                buffer_real = i * cos_m_phi(i) * p_mn(i, ii) / (sin_theta * rho)
+                                buffer_real = i * cos_m_phi(i) * p_mn_divided_by_sin_theta(i, ii) / rho
                                 buffer_cmplx = buffer_real * r_d_cmplx(ii)
                                 N_omn(i)%coordinate(ii)%phi = buffer_cmplx
                             end do
@@ -379,6 +385,290 @@ module lib_mie_vector_spherical_harmonics
 
 
         end subroutine lib_mie_vector_spherical_harmonics_components_real
+
+        ! calculation of the components of the vector spherical harmonic
+        !
+        ! Argument
+        ! ----
+        !   theta: double precision
+        !       polar angle
+        !   phi: double precision
+        !       azimuthal angle
+        !   rho: double precision
+        !       dimensionless varibale rho = k*r
+        !       k: wavenumber
+        !       r: distance
+        !   m: intger, dimension(2)
+        !       first element: start index
+        !       second element: last index
+        !       HINT: first element .le. second element
+        !   n: integer, dimension(2)
+        !       first element: start index
+        !       second element: last index
+        !       HINT: first element .le. second element
+        !   z_selector: integer
+        !       1: spherical Bessel function first kind   j_n
+        !       2: spherical Bessel function second kind  y_n
+        !       3: spherical Hankel function first kind   h^(1)_n
+        !       4: spherical Hankel function second kind  h^(2)_n
+        !
+        ! Results
+        ! ----
+        !   rv: complex, dimension(3)
+        !       values of the spherical coordinates (rho, theta, phi)
+        !
+        ! LaTeX: $$ \begin{aligned} \mathbf{M}_{e m n}=& \frac{-m}{\sin \theta} \sin m \phi P_{n}^{m}(\cos \theta) z_{n}(\rho) \hat{\mathbf{e}}_{\theta} \\ &-\cos m \phi \frac{d P_{n}^{m}(\cos \theta)}{d \theta} z_{n}(\rho) \hat{\mathbf{e}}_{\phi} \end{aligned} $$
+        !
+        ! Reference: Absorption and Scattering of Light by Small Particles, eq. 4.17
+        subroutine lib_mie_vector_spherical_harmonics_components_cmplx(theta, phi, rho, m, n, z_selector, &
+                                                      M_emn, M_omn, N_emn, N_omn, &
+                                                      not_calc_Memn, not_calc_Momn, not_calc_Nemn, not_calc_Nomn)
+            implicit none
+            ! dummy
+            double precision, intent(in) :: theta
+            double precision, intent(in) :: phi
+            complex(kind=8), intent(in) :: rho
+            integer(kind=VECTOR_SPHERICAL_HARMONICS_COMPONENT_NUMBER_KIND), dimension(2) :: m
+            integer(kind=VECTOR_SPHERICAL_HARMONICS_COMPONENT_NUMBER_KIND), dimension(2) :: n
+            integer(kind=1) :: z_selector
+
+            type(list_spherical_coordinate_cmplx_type), dimension(:), allocatable, intent(inout) :: M_emn
+            type(list_spherical_coordinate_cmplx_type), dimension(:), allocatable, intent(inout) :: M_omn
+            type(list_spherical_coordinate_cmplx_type), dimension(:), allocatable, intent(inout) :: N_emn
+            type(list_spherical_coordinate_cmplx_type), dimension(:), allocatable, intent(inout) :: N_omn
+
+            logical, optional :: not_calc_Memn
+            logical, optional :: not_calc_Momn
+            logical, optional :: not_calc_Nemn
+            logical, optional :: not_calc_Nomn
+
+            ! auxiliary
+            integer(kind=VECTOR_SPHERICAL_HARMONICS_COMPONENT_NUMBER_KIND) :: i
+            integer(kind=VECTOR_SPHERICAL_HARMONICS_COMPONENT_NUMBER_KIND) :: ii
+            integer(kind=VECTOR_SPHERICAL_HARMONICS_COMPONENT_NUMBER_KIND) :: number_of_members_m
+            integer(kind=VECTOR_SPHERICAL_HARMONICS_COMPONENT_NUMBER_KIND) :: number_of_members_n
+
+            double precision, dimension(m(2)-m(1)+1, n(2)-n(1)+1) :: p_mn
+            double precision, dimension(m(2)-m(1)+1, n(2)-n(1)+1) :: p_dmn
+            double precision, dimension(m(2)-m(1)+1, n(2)-n(1)+1) :: p_mn_divided_by_sin_theta
+            double precision, dimension(0:n(2)) :: buffer_p_n
+            double precision, dimension(0:n(2)) :: buffer_p_dn
+
+            complex(kind=8), dimension(n(2)-n(1)+1) :: z_n_cmplx
+            complex(kind=8), dimension(n(2)-n(1)+1) :: z_d_cmplx ! deriviative
+
+            complex(kind=8), dimension(n(2)-n(1)+1) :: z_divided_by_rho_cmplx
+
+            ! Riccati-Bessel
+            complex(kind=8), dimension(n(2)-n(1)+1) :: r_cmplx
+            complex(kind=8), dimension(n(2)-n(1)+1) :: r_d_cmplx ! deriviative
+
+            double precision :: cos_theta
+            double precision :: sin_theta
+            double precision :: minus_sin_theta
+            double precision, dimension(m(2)-m(1)+1) :: m_divided_by_sin_theta
+
+            double precision, dimension(n(2)-n(1)+1) :: cos_m_phi
+            double precision, dimension(n(2)-n(1)+1) :: sin_m_phi
+
+
+            logical :: m_not_calc_Memn
+            logical :: m_not_calc_Momn
+            logical :: m_not_calc_Nemn
+            logical :: m_not_calc_Nomn
+
+            double precision :: buffer_real
+            complex(kind=8) :: buffer_cmplx
+
+
+            number_of_members_m = m(2) - m(1) + 1
+            number_of_members_n = n(2) - n(1) + 1
+
+            ! --- standard value ---
+            m_not_calc_Memn = .false.
+            m_not_calc_Momn = .false.
+            m_not_calc_Nemn = .false.
+            m_not_calc_Nomn = .false.
+
+            if (present(not_calc_Memn)) then
+                m_not_calc_Memn = not_calc_Memn
+            end if
+
+            if (present(not_calc_Momn)) then
+                m_not_calc_Momn = not_calc_Momn
+            end if
+
+            if (present(not_calc_Nemn)) then
+                m_not_calc_Nemn = not_calc_Nemn
+            end if
+            if (present(not_calc_Nomn)) then
+                m_not_calc_Nomn = not_calc_Nomn
+            end if
+
+            ! --- init ---
+            if (.not. m_not_calc_Memn) then
+                allocate(M_emn(m(2)-m(1)+1))
+                do i=1, number_of_members_m
+                    allocate (M_emn(i)%coordinate(number_of_members_n))
+                end do
+            end if
+
+            if (.not. m_not_calc_Momn) then
+                allocate(M_omn(m(2)-m(1)+1))
+                do i=1, number_of_members_m
+                    allocate (M_omn(i)%coordinate(number_of_members_n))
+                end do
+            end if
+
+            if (.not. m_not_calc_Nemn) then
+                allocate(N_emn(m(2)-m(1)+1))
+                do i=1, number_of_members_m
+                    allocate (N_emn(i)%coordinate(number_of_members_n))
+                end do
+            end if
+
+            if (.not. m_not_calc_Nemn) then
+                allocate(N_omn(m(2)-m(1)+1))
+                do i=1, number_of_members_m
+                    allocate (N_omn(i)%coordinate(number_of_members_n))
+                end do
+            end if
+
+
+            ! --- pre-calculation ---
+            cos_theta = cos(theta)
+            sin_theta = sin(theta)
+            minus_sin_theta = -sin(theta)
+
+
+            do i=1, number_of_members_m
+                cos_m_phi(i) = cos(i*phi)
+                sin_m_phi(i) = sin(i*phi)
+                m_divided_by_sin_theta(i) = i / sin_theta
+            end do
+
+            select case (z_selector)
+                case(1)
+                    ! spherical Bessel function first kind j_n
+                    ! internal: calculation with Riccati-Bessel functions: S_n
+                    r_d_cmplx = lib_math_riccati_s_derivative(rho, n(1), number_of_members_n, r_cmplx)
+                    z_n_cmplx = r_cmplx / rho
+                case(2)
+                    ! spherical Bessel function second kind y_n
+                    ! internal: calculation with Riccati-Bessel functions: C_n
+                    r_d_cmplx = lib_math_riccati_c_derivative(rho, n(1), number_of_members_n, r_cmplx)
+                    z_n_cmplx = r_cmplx / rho
+                case(3)
+                    ! spherical Hankel function first kind   h^(1)_n
+                    ! internal: calculation with Riccati-Bessel functions: Xi_n
+                    r_d_cmplx = lib_math_riccati_xi_derivative(rho, n(1), number_of_members_n, r_cmplx)
+                    z_n_cmplx = r_cmplx / rho
+                case(4)
+                    ! spherical Hankel function first kind   h^(2)_n
+                    ! internal: calculation with Riccati-Bessel functions: Zeta_n
+                    r_d_cmplx = lib_math_riccati_zeta_derivative(rho, n(1), number_of_members_n, r_cmplx)
+                    z_n_cmplx = r_cmplx / rho
+                case default
+                    z_n_cmplx = cmplx(0,0)
+                    z_d_cmplx = cmplx(0,0)
+
+                    r_cmplx = cmplx(0,0)
+                    r_d_cmplx = cmplx(0,0)
+                    print*, "lib_mie_vector_spherical_harmonics_M_emn: ERROR"
+                    print*, "  undefined z_selector value: ", z_selector
+                    return
+            end select
+
+            do i=1, number_of_members_m
+                call lib_math_associated_legendre_polynomial_theta(theta, i, n(2), buffer_p_n,  buffer_p_dn, .false.)
+                p_mn_divided_by_sin_theta(i, :) = buffer_p_n(n(1):n(2))
+                ! final step of the calculation of >>> derivation[LegendreP[l, m, cos (x)], x]
+                p_dmn(i, :) = minus_sin_theta * buffer_p_dn(n(1):n(2))
+
+                call lib_math_associated_legendre_polynomial(sin_theta, i, n(2), buffer_p_n,  buffer_p_dn, .false.)
+                p_mn(i, :) = buffer_p_n(n(1):n(2))
+            end do
+
+
+            ! --- calculations of the components M and N ---
+            ! M_emn
+            ! eq. (4.17)
+            if (.not. m_not_calc_Memn) then
+                do i=1, number_of_members_m
+                    do ii=1, number_of_members_n
+                        buffer_real = -i * sin_m_phi(i) * p_mn_divided_by_sin_theta(i, ii)
+                        buffer_cmplx = buffer_real * z_n_cmplx(ii)
+                        M_emn(i)%coordinate(ii)%theta = buffer_cmplx
+
+                        buffer_real = - cos_m_phi(i) * p_dmn(i, ii)
+                        buffer_cmplx = buffer_real * z_n_cmplx(ii)
+                        M_emn(i)%coordinate(ii)%phi = buffer_cmplx
+
+                        M_emn(i)%coordinate(ii)%rho = cmplx(0,0, kind=8)
+                    end do
+                end do
+            end if
+
+            ! M_omn
+            ! eq. (4.18)
+            if (.not. m_not_calc_Momn) then
+                do i=1, number_of_members_m
+                    do ii=1, number_of_members_n
+                        buffer_real = i * cos_m_phi(i) * p_mn_divided_by_sin_theta(i, ii)
+                        buffer_cmplx = buffer_real * z_n_cmplx(ii)
+                        M_omn(i)%coordinate(ii)%theta = buffer_cmplx
+
+                        buffer_real = - sin_m_phi(i) * p_dmn(i, ii)
+                        buffer_cmplx = buffer_real * z_n_cmplx(ii)
+                        M_omn(i)%coordinate(ii)%phi = buffer_cmplx
+
+                        M_omn(i)%coordinate(ii)%rho = cmplx(0,0, kind=8)
+                    end do
+                end do
+            end if
+
+            ! N_emn
+            ! eq. (4.19)
+            if (.not. m_not_calc_Nemn) then
+                do i=1, number_of_members_m
+                    do ii=1, number_of_members_n
+                        buffer_real = cos_m_phi(i) * ii*(ii+1) * p_mn(i, ii)
+                        buffer_cmplx = z_divided_by_rho_cmplx(ii) * buffer_real
+                        N_emn(i)%coordinate(ii)%rho = buffer_cmplx
+
+                        buffer_real = cos_m_phi(i) * p_dmn(i, ii)
+                        buffer_cmplx = buffer_real * r_d_cmplx(ii) / rho
+                        N_emn(i)%coordinate(ii)%theta = buffer_cmplx
+
+                        buffer_real = - i * sin_m_phi(i) * p_mn_divided_by_sin_theta(i, ii)
+                        buffer_cmplx = buffer_real * r_d_cmplx(ii) / rho
+                        N_emn(i)%coordinate(ii)%phi = buffer_cmplx
+                    end do
+                end do
+            end if
+
+           ! N_omn
+           ! eq. (4.20)
+            if (.not. m_not_calc_Nomn) then
+                do i=1, number_of_members_m
+                    do ii=1, number_of_members_n
+                        buffer_real = sin_m_phi(i) * ii*(ii+1) * p_mn(i, ii)
+                        buffer_cmplx = z_divided_by_rho_cmplx(ii) * buffer_real
+                        N_omn(i)%coordinate(ii)%rho = buffer_cmplx
+
+                        buffer_real = sin_m_phi(i) * p_dmn(i, ii)
+                        buffer_cmplx = buffer_real * r_d_cmplx(ii) / rho
+                        N_omn(i)%coordinate(ii)%theta = buffer_cmplx
+
+                        buffer_real = i * cos_m_phi(i) * p_mn_divided_by_sin_theta(i, ii)
+                        buffer_cmplx = buffer_real * r_d_cmplx(ii) / rho
+                        N_omn(i)%coordinate(ii)%phi = buffer_cmplx
+                    end do
+                end do
+            end if
+
+
+        end subroutine lib_mie_vector_spherical_harmonics_components_cmplx
 
 
         function lib_mie_vector_spherical_harmonics_test_functions() result (rv)
