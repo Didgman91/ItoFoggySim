3147b3f1534b7aafb68c3e9b4d1283ded6809fb6
itodaiber <st147783@stud.uni-stuttgart.de>
2019-09-03 16:16:01 +0200

    apply new generated functions: tau and pi
    pi= m * LegendreP[n,m,Cos[theta]] / Sin[theta]
    tau = D[LegendreP[n,m,Cos[theta]], x]

--- libmath_generated_functions/src/lib/libmath_generated_functions.f90
+++ libmath_generated_functions/src/lib/libmath_generated_functions.f90
@@ -1,7 +1,7 @@
 module libmath_generated_functions
-    use poly_new
+    use PiPoly
     use limit_new
-    use poly_derivative_new
+    use TauPoly
     use poly_derivative_limit_new
     implicit none
 
