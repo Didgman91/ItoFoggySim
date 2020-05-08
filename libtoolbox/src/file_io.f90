a7d0f9f3c804262c47679cb5b3ab19edab66d5e0
itodaiber <st147783@stud.uni-stuttgart.de>
2020-05-08 15:02:43 +0200

    add function with single precision arguments; add RGB export

--- libtoolbox/src/file_io.f90
+++ libtoolbox/src/file_io.f90
@@ -52,6 +52,18 @@ module file_io
         module procedure file_io_csv_run_get_double
     end interface
 
+    interface write_ppm_p3
+        module procedure write_ppm_p3_single
+        module procedure write_ppm_p3_RGB_single
+
+        module procedure write_ppm_p3_double
+        module procedure write_ppm_p3_RGB_double
+    end interface
+
+    interface color_map_temperature
+        module procedure color_map_temperature_single
+        module procedure color_map_temperature_double
+    end interface
 
     ! parameter
     integer, parameter :: MAX_LINES = 65535
@@ -123,12 +135,339 @@ module file_io
     ! Argument
     !   u: integer
     !       >>> open(unit=u, file=trim(filename), status='new')
+    !   imgR: double precision, 2D array
+    !       red channel
+    !   imgG: double precision, 2D array
+    !       green channel
+    !   imgB: double precision, 2D array
+    !       blue channel
+    !   rescale: logical, opional (std = .false.)
+    !       rescales the pixel values to 0-255
+    !   hdr: logical, optional (std = .false.)
+    !       use high dynamic range
+    !
+    function write_ppm_p3_RGB_single(u, imgR, imgG, imgB, rescale, hdr) result (rv)
+        implicit none
+        ! dummy
+        integer, intent(in) :: u
+        real, dimension(:, :), intent(inout) :: imgR
+        real, dimension(lbound(imgR,1):ubound(imgR,1), lbound(imgR,2):ubound(imgR,2)), intent(inout) :: imgG
+        real, dimension(lbound(imgR,1):ubound(imgR,1), lbound(imgR,2):ubound(imgR,2)), intent(inout) :: imgB
+        logical, intent(in), optional :: rescale
+        logical, intent(in), optional :: hdr
+
+        logical :: rv
+
+        ! parameter
+        character(len=*), parameter :: formatstr = '(10(I3, x, I3, x, I3, x))'
+        integer(kind=2), parameter :: max_value = 255
+
+        ! auxiliary
+        logical :: m_rescale
+        logical :: m_hdr
+
+        integer :: i
+        integer :: ii
+        double precision :: img_max_value
+        double precision :: img_min_value
+
+        integer(kind=2), dimension(size(imgR, 1)*3, size(imgR, 2)) :: img_discretised_color
+
+        integer(kind=2), dimension(3) :: buffer
+
+        m_rescale = .false.
+        if (present(rescale)) m_rescale = rescale
+
+        m_hdr = .false.
+        if (present(hdr)) m_hdr = hdr
+
+        img_max_value = max(maxval(imgR), maxval(imgG), maxval(imgB))
+        img_min_value = min(minval(imgR), minval(imgG), minval(imgB))
+
+        write(u, '(A)') "P3"
+        write(u, '(I6.1, X, I6.1)') size(imgR, 1), size(imgR, 2)
+        write(u, '(I3)') max_value
+        write(u, '(A)') "# The part above is the header"
+        write(u, '(A)') "# P3   means this is a RGB color image in ASCII"
+        write(u, '(A, I6.1, A, I6.1, A)') "# ", size(imgR, 1), " x ", size(imgR, 2), &
+                                          "  is the width and height of the image in pixels"
+        write(u, '(A, I3)') "# pixel value range for each color: 0 - ", max_value
+        write(u, '(A, EN17.2, A, EN17.2)') "# data point value range: ", img_min_value, " - ", img_max_value
+        write(u, '(A)') "# The part below is image data: RGB triplets"
+
+        rv = .true.
+
+        !$OMP PARALLEL DO PRIVATE(i, ii, buffer)
+        do i=1, size(imgR, 1)
+            do ii=1, size(imgR, 2)
+                if (m_hdr) then
+                    ! reference: Karsten Frenner: F2
+                    ! red
+                    img_discretised_color(3*(i-1)+1, ii) = int(log(imgR(i, ii) + 1) / log(img_max_value) * max_value, kind=2)
+                    ! green
+                    img_discretised_color(3*(i-1)+2, ii) = int(log(imgG(i, ii) + 1) / log(img_max_value) * max_value, kind=2)
+                    ! blue
+                    img_discretised_color(3*(i-1)+3, ii) = int(log(imgB(i, ii) + 1) / log(img_max_value) * max_value, kind=2)
+                else if (m_rescale) then
+                    ! red
+                    img_discretised_color(3*(i-1)+1, ii) = int((imgR(i, ii)-img_min_value) / img_max_value * max_value, kind=2)
+                    ! green
+                    img_discretised_color(3*(i-1)+2, ii) = int((imgG(i, ii)-img_min_value) / img_max_value * max_value, kind=2)
+                    ! blue
+                    img_discretised_color(3*(i-1)+3, ii) = int((imgB(i, ii)-img_min_value) / img_max_value * max_value, kind=2)
+                else
+                    if (imgR(i,ii) .gt. 255) then
+                        ! red
+                        img_discretised_color(3*(i-1)+1, ii) = int(max_value, 2)
+                    else if (imgR(i,ii) .lt. 0) then
+                        ! red
+                        img_discretised_color(3*(i-1)+1, ii) = int(0, 2)
+                    else
+                        ! red
+                        img_discretised_color(3*(i-1)+1, ii) = int(imgR(i, ii), 2)
+                    end if
+
+                    if (imgG(i,ii) .gt. 255) then
+                        ! green
+                        img_discretised_color(3*(i-1)+2, ii) = int(max_value, 2)
+                    else if (imgG(i,ii) .lt. 0) then
+                        ! green
+                        img_discretised_color(3*(i-1)+2, ii) = int(0, 2)
+                    else
+                        ! green
+                        img_discretised_color(3*(i-1)+2, ii) = int(imgG(i, ii), 2)
+                    end if
+
+                    if (imgB(i,ii) .gt. 255) then
+                        ! blue
+                        img_discretised_color(3*(i-1)+3, ii) = int(max_value, 2)
+                    else if (imgB(i,ii) .lt. 0) then
+                        ! blue
+                        img_discretised_color(3*(i-1)+3, ii) = int(0, 2)
+                    else
+                        ! blue
+                        img_discretised_color(3*(i-1)+3, ii) = int(imgB(i, ii), 2)
+                    end if
+                end if
+            end do
+        end do
+        !$OMP END PARALLEL DO
+
+        if (rv) then
+            do i=1, size(img_discretised_color, 2)
+                write(u, formatstr) img_discretised_color(:, i)
+            end do
+        end if
+
+    end function write_ppm_p3_RGB_single
+
+    ! writes two columns into a ppm file
+    !
+    ! Argument
+    !   u: integer
+    !       >>> open(unit=u, file=trim(filename), status='new')
+    !   imgR: double precision, 2D array
+    !       red channel
+    !   imgG: double precision, 2D array
+    !       green channel
+    !   imgB: double precision, 2D array
+    !       blue channel
+    !   rescale: logical, opional (std = .false.)
+    !       rescales the pixel values to 0-255
+    !   hdr: logical, optional (std = .false.)
+    !       use high dynamic range
+    !
+    function write_ppm_p3_RGB_double(u, imgR, imgG, imgB, rescale, hdr) result (rv)
+        implicit none
+        ! dummy
+        integer, intent(in) :: u
+        double precision, dimension(:, :), intent(inout) :: imgR
+        double precision, dimension(lbound(imgR,1):ubound(imgR,1), lbound(imgR,2):ubound(imgR,2)), intent(inout) :: imgG
+        double precision, dimension(lbound(imgR,1):ubound(imgR,1), lbound(imgR,2):ubound(imgR,2)), intent(inout) :: imgB
+        logical, intent(in), optional :: rescale
+        logical, intent(in), optional :: hdr
+
+        logical :: rv
+
+        ! parameter
+        character(len=*), parameter :: formatstr = '(10(I3, x, I3, x, I3, x))'
+        integer(kind=2), parameter :: max_value = 255
+
+        ! auxiliary
+        logical :: m_rescale
+        logical :: m_hdr
+
+        integer :: i
+        integer :: ii
+        double precision :: img_max_value
+        double precision :: img_min_value
+
+        integer(kind=2), dimension(size(imgR, 1)*3, size(imgR, 2)) :: img_discretised_color
+
+        integer(kind=2), dimension(3) :: buffer
+
+        m_rescale = .true.
+        if (present(rescale)) m_rescale = rescale
+
+        m_hdr = .false.
+        if (present(hdr)) m_hdr = hdr
+
+        img_max_value = max(maxval(imgR), maxval(imgG), maxval(imgB))
+        img_min_value = min(minval(imgR), minval(imgG), minval(imgB))
+
+        write(u, '(A)') "P3"
+        write(u, '(I6.1, X, I6.1)') size(imgR, 1), size(imgR, 2)
+        write(u, '(I3)') max_value
+        write(u, '(A)') "# The part above is the header"
+        write(u, '(A)') "# P3   means this is a RGB color image in ASCII"
+        write(u, '(A, I6.1, A, I6.1, A)') "# ", size(imgR, 1), " x ", size(imgR, 2), &
+                                          "  is the width and height of the image in pixels"
+        write(u, '(A, I3)') "# pixel value range for each color: 0 - ", max_value
+        write(u, '(A, EN17.2, A, EN17.2)') "# data point value range: ", img_min_value, " - ", img_max_value
+        write(u, '(A)') "# The part below is image data: RGB triplets"
+
+        rv = .true.
+
+        !$OMP PARALLEL DO PRIVATE(i, ii, buffer)
+        do i=1, size(imgR, 1)
+            do ii=1, size(imgR, 2)
+                if (m_hdr) then
+                    ! reference: Karsten Frenner: F2
+                    ! red
+                    img_discretised_color(3*(i-1)+1, ii) = int(log(imgR(i, ii) + 1) / log(img_max_value) * max_value, kind=2)
+                    ! green
+                    img_discretised_color(3*(i-1)+2, ii) = int(log(imgG(i, ii) + 1) / log(img_max_value) * max_value, kind=2)
+                    ! blue
+                    img_discretised_color(3*(i-1)+3, ii) = int(log(imgB(i, ii) + 1) / log(img_max_value) * max_value, kind=2)
+                else if (m_rescale) then
+                    ! red
+                    img_discretised_color(3*(i-1)+1, ii) = int((imgR(i, ii)-img_min_value) / img_max_value * max_value, kind=2)
+                    ! green
+                    img_discretised_color(3*(i-1)+2, ii) = int((imgG(i, ii)-img_min_value) / img_max_value * max_value, kind=2)
+                    ! blue
+                    img_discretised_color(3*(i-1)+3, ii) = int((imgB(i, ii)-img_min_value) / img_max_value * max_value, kind=2)
+                else
+                    if (imgR(i,ii) .gt. 255) then
+                        ! red
+                        img_discretised_color(3*(i-1)+1, ii) = int(max_value, 2)
+                    else if (imgR(i,ii) .lt. 0) then
+                        ! red
+                        img_discretised_color(3*(i-1)+1, ii) = int(0, 2)
+                    else
+                        ! red
+                        img_discretised_color(3*(i-1)+1, ii) = int(imgR(i, ii), 2)
+                    end if
+
+                    if (imgG(i,ii) .gt. 255) then
+                        ! green
+                        img_discretised_color(3*(i-1)+2, ii) = int(max_value, 2)
+                    else if (imgG(i,ii) .lt. 0) then
+                        ! green
+                        img_discretised_color(3*(i-1)+2, ii) = int(0, 2)
+                    else
+                        ! green
+                        img_discretised_color(3*(i-1)+2, ii) = int(imgG(i, ii), 2)
+                    end if
+
+                    if (imgB(i,ii) .gt. 255) then
+                        ! blue
+                        img_discretised_color(3*(i-1)+3, ii) = int(max_value, 2)
+                    else if (imgB(i,ii) .lt. 0) then
+                        ! blue
+                        img_discretised_color(3*(i-1)+3, ii) = int(0, 2)
+                    else
+                        ! blue
+                        img_discretised_color(3*(i-1)+3, ii) = int(imgB(i, ii), 2)
+                    end if
+                end if
+            end do
+        end do
+        !$OMP END PARALLEL DO
+
+        if (rv) then
+            do i=1, size(img_discretised_color, 2)
+                write(u, formatstr) img_discretised_color(:, i)
+            end do
+        end if
+
+    end function write_ppm_p3_RGB_double
+
+    ! writes two columns into a ppm file
+    !
+    ! Argument
+    !   u: integer
+    !       >>> open(unit=u, file=trim(filename), status='new')
     !   img: double precision, 2D array
     !   color_map: integer, opional (std = 1)
     !       - 1: temperatur
     !
     !
-    function write_ppm_p3(u, img, color_map, logarithmic) result (rv)
+    function write_ppm_p3_single(u, img, color_map, logarithmic) result (rv)
+        implicit none
+        ! dummy
+        integer, intent(in) :: u
+        real, dimension(:, :), intent(inout) :: img
+        integer, intent(in), optional :: color_map
+        logical, intent(in), optional :: logarithmic
+
+        logical :: rv
+
+        ! parameter
+        character(len=*), parameter :: formatstr = '(10(I3, x, I3, x, I3, x))'
+        integer(kind=2), parameter :: max_value = 255
+
+        ! auxiliary
+        integer :: i
+        double precision :: img_max_value
+        double precision :: img_min_value
+
+        integer(kind=2), dimension(size(img, 1)*3, size(img, 2)) :: img_discretised_color
+
+        img_max_value = maxval(img)
+        img_min_value = minval(img)
+
+        write(u, '(A)') "P3"
+        write(u, '(I6.1, X, I6.1)') size(img, 1), size(img, 2)
+        write(u, '(I3)') max_value
+        write(u, '(A)') "# The part above is the header"
+        write(u, '(A)') "# P3   means this is a RGB color image in ASCII"
+        write(u, '(A, I6.1, A, I6.1, A)') "# ", size(img, 1), " x ", size(img, 2), &
+                                          "  is the width and height of the image in pixels"
+        write(u, '(A, I3)') "# pixel value range for each color: 0 - ", max_value
+        write(u, '(A, EN17.2, A, EN17.2)') "# data point value range: ", img_min_value, " - ", img_max_value
+        write(u, '(A)') "# The part below is image data: RGB triplets"
+
+        rv = .true.
+
+        if (present(color_map)) then
+            if (color_map .eq. 1) then
+                 call color_map_temperature(img, max_value, img_discretised_color, logarithmic=logarithmic)
+            else
+                rv = .false.
+            end if
+        else
+            call color_map_temperature(img, max_value, img_discretised_color, logarithmic=logarithmic)
+        end if
+
+        if (rv) then
+            do i=1, size(img_discretised_color, 2)
+                write(u, formatstr) img_discretised_color(:, i)
+            end do
+        end if
+
+    end function write_ppm_p3_single
+
+    ! writes two columns into a ppm file
+    !
+    ! Argument
+    !   u: integer
+    !       >>> open(unit=u, file=trim(filename), status='new')
+    !   img: double precision, 2D array
+    !   color_map: integer, opional (std = 1)
+    !       - 1: temperatur
+    !
+    !
+    function write_ppm_p3_double(u, img, color_map, logarithmic) result (rv)
         implicit none
         ! dummy
         integer, intent(in) :: u
@@ -181,9 +520,77 @@ module file_io
             end do
         end if
 
-    end function write_ppm_p3
+    end function write_ppm_p3_double
 
-    subroutine color_map_temperature(img, max_value, rv, logarithmic)
+    subroutine color_map_temperature_single(img, max_value, rv, logarithmic)
+        implicit none
+        ! dummy
+        real, dimension(:, :), intent(in) :: img
+        integer(kind=2), intent(in) :: max_value
+        logical, intent(in), optional :: logarithmic
+
+        integer(kind=2), dimension(size(img, 1)*3, size(img, 2)), intent(inout) :: rv
+
+        ! auxiliary
+        integer :: i
+        integer :: ii
+        double precision :: img_max_abs_value
+        integer(kind=2) :: buffer
+
+        img_max_abs_value = max(abs(maxval(img)), abs(minval(img)))
+
+        !$OMP PARALLEL DO PRIVATE(i, ii, buffer)
+        do i=1, size(img, 1)
+            do ii=1, size(img, 2)
+                if (img(i, ii) .gt. 0) then
+                    if (present(logarithmic)) then
+                        if (logarithmic) then
+                            buffer = int( log(img(i,ii)+1) * real(max_value) / log(img_max_abs_value+1), kind=2)
+                        else
+                            buffer = int( img(i,ii) * real(max_value) / img_max_abs_value, kind=2)
+                        end if
+                    else
+                        buffer = int( img(i,ii) * real(max_value) / img_max_abs_value, kind=2)
+                    end if
+
+                    ! red
+                    rv(3*(i-1)+1, ii) = max_value
+                    ! green
+                    rv(3*(i-1)+2, ii) = max_value - buffer
+                    ! blue
+                    rv(3*(i-1)+3, ii) = max_value - buffer
+                else if (img(i, ii) .eq. 0) then
+                    ! red
+                    rv(3*(i-1)+1, ii) = max_value
+                    ! green
+                    rv(3*(i-1)+2, ii) = max_value
+                    ! blue
+                    rv(3*(i-1)+3, ii) = max_value
+                else
+                    if (present(logarithmic)) then
+                        if (logarithmic) then
+                            buffer = int( log(-img(i,ii)+1) * real(max_value) / log(img_max_abs_value+1), kind=2)
+                        else
+                            buffer = int( -img(i,ii) * real(max_value) / img_max_abs_value, kind=2)
+                        end if
+                    else
+                        buffer = int( -img(i,ii) * real(max_value) / img_max_abs_value, kind=2)
+                    end if
+
+!                    buffer = int( img(i,ii) * real(max_value) / img_min_value, kind=2)
+                    ! red
+                    rv(3*(i-1)+1, ii) = max_value - buffer
+                    ! green
+                    rv(3*(i-1)+2, ii) = max_value - buffer
+                    ! blue
+                    rv(3*(i-1)+3, ii) = max_value
+                end if
+            end do
+        end do
+        !$OMP END PARALLEL DO
+    end subroutine
+
+    subroutine color_map_temperature_double(img, max_value, rv, logarithmic)
         implicit none
         ! dummy
         double precision, dimension(:, :), intent(in) :: img
